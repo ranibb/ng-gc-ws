fad636a109947b28e67958b27af8513084302f37
Rani Abu Sultan <raniabusultan@gmail.com>
2025-02-25 14:24:31 +0300

    Generate an Angular publishable library with Tailwind CSS configured run + Add Tailwind CSS to the Angular application (demo app)

---
+++ libs/ng-gc/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/ng-gc/ng-gc.component';
---
+++ libs/ng-gc/src/lib/ng-gc/ng-gc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ng-gc-ng-gc',
+  imports: [CommonModule],
+  templateUrl: './ng-gc.component.html',
+  styleUrl: './ng-gc.component.css',
+})
+export class NgGcComponent {}
